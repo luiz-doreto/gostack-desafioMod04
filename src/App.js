6c32f62c026e2aa0d7e18e4be9d59cd21529f67f
Luiz Doreto <luizdoreto@Luizs-MacBook-Air.local>
2019-10-24 23:01:42 -0300

    Desafio 4 - Iniciando desafio

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Facebook from './components/facebook/Facebook';
+
+import './App.css';
+
+function App() {
+    return <Facebook />;
+}
+
+export default App;
---
+++ src/components/facebook/Facebook.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './header/Header';
+
+class Facebook extends Component {
+    render() {
+        return (
+            <>
+                <Header />
+            </>
+        );
+    }
+}
+
+export default Facebook;
